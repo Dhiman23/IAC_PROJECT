53ca41727a8dc3a453af6e97d749cb637f4ce43b
Dhiman23 <sajaldhiman68@gmail.com>
2024-09-07 19:35:10 +0530

    ansible playbook

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "web-sg-1" {
   dynamic "ingress" {
-    for_each = vars.port
+    for_each = var.ports
     content {
       from_port   = ingress.value
       to_port     = ingress.value
@@ -17,10 +17,10 @@ resource "aws_security_group" "web-sg-1" {
   }
 }
 
-resource "aws_instance" "jenkins-server" {
+resource "aws_instance" "jenkins" {
   ami             = "ami-0e86e20dae9224db8"
-  instance_type   = "t3.medium"
-  key_name        = "test"
+  instance_type   = "t2.medium"
+  key_name        = "imp"
   security_groups = [aws_security_group.web-sg-1.name]
   user_data       = templatefile("./installConfig.sh",{})
 
